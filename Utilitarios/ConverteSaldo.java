ee704f31eb5187f62705311905a36e5ab8d81fe7
Gabriel Nascimento Borges Pereira <84687003+gabrielnbp@users.noreply.github.com>
2023-01-19 07:23:30 -0300

    Add files via upload

---
+++ Utilitarios/ConverteSaldo.java
@@ -0,0 +1,13 @@
+package Utilitarios;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class ConverteSaldo {
+	
+	static NumberFormat formatandoValores = new DecimalFormat("R$ #,##0.00");
+	
+	public static String doubleToString(double valor) {
+		return formatandoValores.format(valor);
+	}
+}
